a5a35aee1e892401929129def33bc5a294a8d0b9
mafervm <mariafernandavilla21@live.com.mx>
2024-03-04 18:46:11 -0600

    Se crearon los componentes para la card de videojuegos, con consola, precio y boton

---
+++ app/src/main/java/com/example/recyclerview/ui/theme/views/GameDataView.kt
@@ -0,0 +1,22 @@
+package com.example.recyclerview.ui.theme.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview (showBackground = true)
+@Composable
+fun GameDataView(){
+    Row(horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth()){
+        Text(text = "PS4")
+        Text(text = "$890 MXN",
+            color = Color(0xFF2C8A1C)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/ui/theme/views/Games.kt
@@ -0,0 +1,57 @@
+package com.example.recyclerview.ui.theme.views
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.recyclerview.R
+
+@Preview(showBackground = true)
+@Composable
+fun CardGame(){
+    Card(modifier = Modifier.padding (8.dp)){
+        Row(modifier = Modifier.fillMaxWidth()){
+            //Imagen
+            Image(
+                modifier = Modifier
+                    .height(170.dp)
+                    .width(120.dp),
+                painter = painterResource(id = R.drawable.minecraft),
+                contentDescription = "ImagenDelJuego",
+                contentScale = ContentScale.Crop
+            )
+            //nombre del juego
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = "Minecraft",
+                    modifier = Modifier.padding(top = 8.dp),
+                    color = Color(0xFFF9A736)
+                )
+                GameDataView()
+                Button(onClick = { }, colors = ButtonDefaults.buttonColors(Color(0xFF6994F9))) {
+                    Text(text = "Comprar")
+                }
+            }
+
+        }
+
+        //consola y precio
+    }
+}
